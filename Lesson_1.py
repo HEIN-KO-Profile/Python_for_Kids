f4a53e27b68333f162f1d6ce21d49b5d302d105c
HEIN KO <teacher.heinko@gmail.com>
2025-10-03 17:42:34 -0700

    First commit - added my project

---
+++ Lesson_1.py
@@ -0,0 +1,5 @@
+#Let's make computer say Hello! and Write my Name
+
+print("Hello, World!")
+print("My name is Hein Ko")
+print("I am learning Python!")
---
+++ Lesson_2.py
@@ -0,0 +1,8 @@
+# Math is Easy!
+# Python is like a super calculator.
+
+print(5+3)    #Addition
+print(10-4)   #Subtraction
+print(6*7)    #Multiplication
+print(20/4)   #Division
+print(2**3)   #Power (2 to the power of 3 or 2^3)
